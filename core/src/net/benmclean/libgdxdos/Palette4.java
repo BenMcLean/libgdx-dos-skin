fddfaa546b595cd1ce0dd0abae85f4913ac8ca4a
Benjamin McLean <mclean.ben@gmail.com>
2017-01-25 19:07:43 -0600

    tweaked shader a tiny bit further

--- core/src/net/benmclean/libgdxdos/Palette4.java
+++ core/src/net/benmclean/libgdxdos/Palette4.java
@@ -34,12 +34,13 @@ public class Palette4 implements Disposable {
             "#else\n" +
             "#define LOWP\n" +
             "#endif\n" +
+            "varying LOWP vec4 v_color;\n" +
             "varying vec2 v_texCoords;\n" +
             "uniform sampler2D u_texPalette;\n" +
             "uniform sampler2D u_texture;\n" +
             "void main() {\n" +
             "   vec4 color = texture2D(u_texture, v_texCoords).rgba;\n" + // on separate line for GWT
-            "	gl_FragColor = texture2D(u_texPalette, vec2(color.r, 0)).rgba;\n" +
+            "	gl_FragColor = v_color * texture2D(u_texPalette, vec2(color.r, 0)).rgba;\n" +
             "}";
 
     public static final String fragmentShaderYieldTransparency = "#ifdef GL_ES\n" +
@@ -53,10 +54,10 @@ public class Palette4 implements Disposable {
             "uniform sampler2D u_texPalette;\n" +
             "uniform sampler2D u_texture;\n" +
             "void main() {\n" +
-            "   vec4 color = texture2D(u_texture, v_texCoords).rgba;\n" + // on separate line for GWT
+            "   vec2 color = texture2D(u_texture, v_texCoords).ra;\n" + // on separate line for GWT
             "	gl_FragColor = v_color * vec4(\n" +
             "       texture2D(u_texPalette, vec2(color.r, 0)).rgb, \n" +
-            "       color.a\n" +
+            "       color.y\n" +
             "   );\n" +
             "}";
 
