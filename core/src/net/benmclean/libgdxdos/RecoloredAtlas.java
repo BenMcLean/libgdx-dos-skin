442ecc1ea41d11cccc60d8b5825f39c6254120ba
Benjamin McLean <mclean.ben@gmail.com>
2017-04-19 18:28:31 -0500

    centered camera, added shader back in

--- core/src/net/benmclean/libgdxdos/RecoloredAtlas.java
+++ core/src/net/benmclean/libgdxdos/RecoloredAtlas.java
@@ -14,31 +14,27 @@ import com.badlogic.gdx.utils.viewport.Viewport;
  * Created by Benjamin on 4/7/2017.
  */
 public class RecoloredAtlas {
-    public static Texture recolor (Texture texture, Palette4 palette) {
-        FrameBuffer frameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, texture.getWidth(), texture.getHeight(), false, false);
+    public static Texture recolor(Texture texture, Palette4 palette) {
+        final int width = texture.getWidth(), height = texture.getHeight();
+        FrameBuffer frameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, width, height, false, false);
         SpriteBatch batch = new SpriteBatch();
-        Viewport viewport = new FitViewport(texture.getWidth(), texture.getHeight());
-
-        viewport.getCamera().position.set(0, 0, 0);
-        viewport.update(texture.getWidth(), texture.getHeight());
+        Viewport viewport = new FitViewport(width, height);
+        viewport.getCamera().position.set(width / 2, height / 2, 0);
+        viewport.update(width, height);
         batch.enableBlending();
-
         frameBuffer.begin();
-        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//        batch.setShader(Palette4.makeShader());
+        batch.setShader(Palette4.makeShader());
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
         viewport.apply();
-        //palette.bind(batch.getShader());
+        palette.bind(batch.getShader());
         batch.draw(texture, 0, 0);
         batch.end();
-//        Texture answer = ScreenUtils.getFrameBufferTexture().getTexture();
-
         Pixmap pixmap = ScreenUtils.getFrameBufferPixmap(0, 0, frameBuffer.getWidth(), frameBuffer.getHeight());
         Texture answer = new Texture(pixmap);
         frameBuffer.end();
-
         answer.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
         batch.dispose();
         frameBuffer.dispose();
