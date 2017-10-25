cff0f53bbc170d192a392b690b85f9787f5a09b1
Benjamin McLean <mclean.ben@gmail.com>
2017-10-24 19:59:36 -0500

    Added repacking atlas using pixmaps at runtime

--- core/src/net/benmclean/libgdxdos/LibGDXDOSGame.java
+++ core/src/net/benmclean/libgdxdos/LibGDXDOSGame.java
@@ -3,11 +3,13 @@ package net.benmclean.libgdxdos;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -16,43 +18,30 @@ public class LibGDXDOSGame extends ApplicationAdapter {
     public static final int VIRTUAL_HEIGHT = 480;
 
     private Skin skin;
-    private Skin defaultSkin;
     private Stage stage;
-    protected ShaderProgram shader;
-    protected Palette4 uiPalette;
-    private boolean applyShader = false;
-    private boolean shaderChanged = true;
-
-    Texture recolored;
 
     @Override
     public void create() {
-        shader = Palette4.makeShader();
-        uiPalette = Palette4.blueUI();
-
-        skin = new Skin(Gdx.files.internal("DOS/uiskin.json"));
-        defaultSkin = new Skin(Gdx.files.internal("default/uiskin.json"));
-
+        //skin = new Skin(Gdx.files.internal("DOS/uiskin.json"));
         stage = new Stage(new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT));
-
-        Texture texture = new Texture(Gdx.files.internal("DOS/uiskin.png"));
-
-        recolored = new Texture(RecoloredAtlas.recolor(texture, uiPalette));
+        skin = new Skin(
+                Gdx.files.internal("DOS/uiskin.json"),
+                RecoloredAtlas.repackAtlas(
+                        new TextureAtlas(Gdx.files.internal("DOS/uiskin.atlas")),
+                        Palette4.blueUI()
+                )
+        );
 
         final VerticalGroup group = new VerticalGroup();
         group.space(16);
 
-        final Image image = new Image(recolored);
-        group.addActor(image);
-
         final CheckBox checkBox = new CheckBox("Apply Shader", skin);
-        checkBox.setChecked(applyShader);
-        checkBox.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                setShader(checkBox.isChecked());
-            }
-        });
+//        checkBox.addListener(new ClickListener() {
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                setShader(checkBox.isChecked());
+//            }
+//        });
         group.addActor(checkBox);
 
         final CheckBox debugCheckBox = new CheckBox("Enable Debug Rendering", skin);
@@ -74,10 +63,6 @@ public class LibGDXDOSGame extends ApplicationAdapter {
 
     @Override
     public void render() {
-        if (shaderChanged) {
-            shade();
-            shaderChanged = false;
-        }
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Gdx.graphics.getDeltaTime());
@@ -87,25 +72,5 @@ public class LibGDXDOSGame extends ApplicationAdapter {
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height);
-    }
-
-    public void setShader(boolean applyShader) {
-        shaderChanged = true;
-        this.applyShader = applyShader;
-    }
-
-    public void shade() {
-        shade(applyShader);
-    }
-
-    public void shade(boolean applyShader) {
-        if (applyShader) {
-            stage.getBatch().setShader(shader);
-            stage.getBatch().begin();
-            uiPalette.bind(stage.getBatch().getShader());
-            stage.getBatch().end();
-        } else {
-            stage.getBatch().setShader(null);
-        }
     }
 }
--- core/src/net/benmclean/libgdxdos/RecoloredAtlas.java
+++ core/src/net/benmclean/libgdxdos/RecoloredAtlas.java
@@ -1,41 +1,84 @@
 package net.benmclean.libgdxdos;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.graphics.g2d.PixmapPacker;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 /**
  * Created by Benjamin on 4/7/2017.
  */
 public class RecoloredAtlas {
-    public static Pixmap recolor(Texture texture, Palette4 palette) {
-        final int width = texture.getWidth(), height = texture.getHeight();
-        FrameBuffer frameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, width, height, false, false);
-        SpriteBatch batch = new SpriteBatch();
-        Viewport viewport = new FitViewport(width, height);
-        viewport.getCamera().position.set(width / 2, height / 2, 0);
-        viewport.update(width, height);
-        batch.enableBlending();
-        frameBuffer.begin();
-        Gdx.gl.glClearColor(0, 0, 0, 0);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setShader(Palette4.makeShader());
-        batch.setProjectionMatrix(viewport.getCamera().combined);
-        batch.begin();
-        viewport.apply();
-        palette.bind(batch.getShader());
-        batch.draw(texture, 0, 0);
-        batch.end();
-        Pixmap answer = ScreenUtils.getFrameBufferPixmap(0, 0, frameBuffer.getWidth(), frameBuffer.getHeight());
-        frameBuffer.end();
-        batch.dispose();
-        frameBuffer.dispose();
-        return answer;
+    public static final int transparent = Color.rgba8888(0f, 0f, 0f, 0f);
+
+    public static TextureAtlas repackAtlas(TextureAtlas atlas, Palette4 palette) {
+        PixmapPacker packer = new PixmapPacker(1024, 1024, Pixmap.Format.RGBA8888, 0, false);
+        packIn(atlas, packer, palette);
+        TextureAtlas textureAtlas = packer.generateTextureAtlas(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, false);
+        for (TextureAtlas.AtlasRegion region : textureAtlas.getRegions()) {
+            TextureAtlas.AtlasRegion raw = atlas.findRegion(region.name);
+            if (raw.pads != null) {
+                region.pads = new int[raw.pads.length];
+                System.arraycopy(raw.pads, 0, region.pads, 0, raw.pads.length);
+            }
+            if (raw.splits != null) {
+                region.splits = new int[raw.splits.length];
+                System.arraycopy(raw.splits, 0, region.splits, 0, raw.splits.length);
+            }
+        }
+        return textureAtlas;
+    }
+
+    public static void packIn(TextureAtlas raw, PixmapPacker packer) {
+        packIn("", raw, packer);
+    }
+
+    public static void packIn(String category, TextureAtlas raw, PixmapPacker packer) {
+        for (TextureAtlas.AtlasRegion region : raw.getRegions())
+            if (region.name.startsWith(category))
+                packIn(region, packer);
+    }
+
+    public static void packIn(TextureAtlas.AtlasRegion region, PixmapPacker packer) {
+        Texture texture = region.getTexture();
+        if (!texture.getTextureData().isPrepared()) texture.getTextureData().prepare();
+        Pixmap pixmap = texture.getTextureData().consumePixmap();
+
+        Pixmap result = new Pixmap(region.getRegionWidth(), region.getRegionHeight(), Pixmap.Format.RGBA8888);
+        for (int x = 0; x < region.getRegionWidth(); x++)
+            for (int y = 0; y < region.getRegionHeight(); y++)
+                result.drawPixel(x, y, pixmap.getPixel(region.getRegionX() + x, region.getRegionY() + y));
+
+        packer.pack(region.toString(), result);
+        texture.dispose();
+    }
+
+    public static void packIn(TextureAtlas raw, PixmapPacker packer, Palette4 palette) {
+        packIn("", raw, packer, palette);
+    }
+
+    public static void packIn(String category, TextureAtlas raw, PixmapPacker packer, Palette4 palette) {
+        for (TextureAtlas.AtlasRegion region : raw.getRegions())
+            if (region.name.startsWith(category))
+                packIn(region, packer, palette);
+    }
+
+    public static void packIn(TextureAtlas.AtlasRegion region, PixmapPacker packer, Palette4 palette) {
+        Texture texture = region.getTexture();
+        if (!texture.getTextureData().isPrepared()) texture.getTextureData().prepare();
+        Pixmap pixmap = texture.getTextureData().consumePixmap();
+        Pixmap result = new Pixmap(region.getRegionWidth(), region.getRegionHeight(), Pixmap.Format.RGBA8888);
+        Color color = new Color();
+        for (int x = 0; x < region.getRegionWidth(); x++)
+            for (int y = 0; y < region.getRegionHeight(); y++) {
+                color.set(pixmap.getPixel(region.getRegionX() + x, region.getRegionY() + y));
+                if (color.a > .05)
+                    result.drawPixel(x, y, Color.rgba8888(palette.get((int) (color.r * 3.9999))));
+                else
+                    result.drawPixel(x, y, transparent);
+            }
+        packer.pack(region.toString(), result);
+        texture.dispose();
     }
 }
